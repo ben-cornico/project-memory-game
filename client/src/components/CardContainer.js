ed4d6b833a6f86982857b56835703470562db9c0
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-07-01 22:54:54 -0700

    cardcontainer update

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -8,36 +8,78 @@ export class CardContainer extends Component {
       loading: true,
       level: 1,
       numImg: 1 * 3,
-      images: null,
+      displayedChar: [],
+      chars: []
     }
+
+    this.trimChars = this.trimChars.bind(this)
   }
 
   async componentDidMount() {
-    const randomPage = Math.floor(Math.random() * 100) + 1;
-    const res = await fetch(`https://api-illustrations.icons8.com/api/v2/illustrations?type=illustration&page=${randomPage}&perPage=${this.state.numImg}&token=${process.env.REACT_APP_icon8_KEY}`)
-    const img = await res.json();
+    const res = await fetch(`http://hp-api.herokuapp.com/api/characters`)
+    const characters = await res.json();
+
+    // this.setState({
+    //   ...this.state,
+    //   chars: characters
+    // })
+
+
+    const listChar = this.trimChars(characters);
     this.setState({
       ...this.state,
-      images: img.illustrations,
-      loading: false,
-    })
+      chars: listChar
+    });
+    console.log(this.state)
+
+    // for(let x=1; x<= this.state.numImg; x++) {
+    //   let randomNum = Math.floor(Math.random() * 402);
+    //   this.setState({
+    //     ...this.state,
+        
+    //     loading: false,
+    //     displayedChar: [...this.state.displayedChar, this.state.chars[randomNum]],
+
+    //   })
+    // }
 
     console.log(this.state)
+    
+  }
+
+  trimChars = (characters) => {
+    const listChars = []
+      characters.map(char => {
+        if(char.image !== "") {
+          listChars.push(char)
+        }
+      })
+      return listChars
+  }
+
+  loadCards = () => {
+
   }
   render() {
     return (
       <div className='card-container'>
-        {
-          this.state.loading || this.state.images ? (
+        {/* {
+          this.state.loading || !this.state.displayedChar ? (
             <Loading />
           ) : (
-            this.state.images.map(image => {
+            this.state.displayedChar.map(char => {
               return (
-                <img srcSet={image.urls.small.url} />
+                <div className="card">
+                  <img srcSet={char.image} alt="" />
+                  <div className="card-name">
+                    {char.name}
+                  </div>
+                </div>
               )
             })
           )
-        }
+          
+        } */}
       </div>
     )
   }
