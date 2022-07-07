39330f668d2db40a48c3b011302e420247ac6bb1
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-07-06 23:03:13 -0700

    ADDED LOADING IMAGES FUNCTION

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -8,59 +8,60 @@ export class CardContainer extends Component {
     this.state = {
       loading: true,
       level: 1,
-      chars: [],
-      displayed: []
+      heroes: this.importAll(require.context('./Dota2heroes', false, /\.(png|jpe?g|svg)$/)),
+      displayed: [],
     }
   }
 
   componentDidMount() {
-    const loadData = async () => {
-      const res = await fetch(`http://hp-api.herokuapp.com/api/characters`);
-      const data = await res.json();
-      return data;
-    }
-
-
-    loadData().then(res => {
-      let list = [];
-      res.map(x => {
-        if(x.image !== "") {
-          list.push(x);
-        }
-      })
-      this.setState({
-        chars: list,
-      })
-    })
-
-    this.loadCards()
+    this.getDisplay()
   }
 
-  loadCards() {
-    
-    const numImage = this.state.level * 3;
-    for(let x=1; x<= numImage; x++) {
-      const randomNum = Math.floor(Math.random() * 24);
-      this.setState({
-        displayed: [...this.state.displayed, this.state.chars[randomNum]]
-      })
+  getDisplay() {
+    const arr = []
+    const numImg = this.state.level * 3;
+    for(let x=1; x <= numImg; x++) {
+      let randomNum = Math.floor(Math.random() * 125)
+      arr.push(this.state.heroes[randomNum])
     }
 
-    console.log(this.state)
     this.setState({
-      loading: false
+      displayed: arr,
     })
   }
+
+  importAll(r) {
+    let images = {};
+    let arr = [];
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+
+
+    //LOOP TO TURN OBJECT INTO ARRAY
+    for(const prop in images) {
+      let wordSplit = prop.split("_");
+      let  joined = wordSplit.join(' ');
+      let pngRemove = joined.split(".png");
+      pngRemove.pop();
+
+
+      arr.push({
+        name: pngRemove,
+        loc: images[prop]
+      })
+    }
+    return arr
+  }
+  
 
   render() {
     return (
       <div className='card-container'>
         {
-          this.state.loading ? (
+          this.state.displayed.length <= 0 ? (
             <Loading />
           ) : (
-            this.state.displayed.map(x => {
-              return (<p>{x.name}</p>)
+            this.state.displayed.map((x) => {
+              return <Card name={x.name} image={x.loc} />
             })
           )
         }
